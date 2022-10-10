e2fdab8326114f2a84978e5bdaf9d75828f2c6a5
ShulgaRuslan <shulgar1988@gmail.com>
2022-10-10 16:18:43 +0300

    update

--- src/services/ImagesAPI.js
+++ src/services/ImagesAPI.js
@@ -6,7 +6,7 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 export const fetchPhotos = async (query, page) => {
   try {
     const response = await axios.get(
-      `?key=24752012-6c87264ae8b83647fd23322b3&page=${page}&q=${query}&image_type=photo&orientation=horizontal&per_page=12`
+      `?key=29657950-ade50d14623a4993a922f2144&page=${page}&q=${query}&image_type=photo&orientation=horizontal&per_page=12`
     );
     const data = await JSON.parse(response.request.response);
     return data;
